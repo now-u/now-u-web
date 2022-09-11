bc8913b2007f62a22fe042a5648c6e7fdb9dc1b6
James Elgar <jamesnelgar@gmail.com>
2022-09-11 19:56:22 +0100

    Cause tiles

---
+++ src/models/cause.tsx
@@ -0,0 +1,7 @@
+export interface Cause {
+    id: number;
+    name: string;
+    image: string;
+    icon: string;
+    description: string;
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,8 @@ module.exports = {
         'blue': '#011A43',
         'black': '#17171A',
         'grey': '#6D7181',
+        'light-grey': '#F7F8FC',
+        'cream': '#FFF3E5',
         'gradlight': '#FD8824',
         'graddark': '#FFDC79',
       }

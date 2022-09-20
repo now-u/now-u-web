7e1102439e6f9a333ef4f6cf54b198fbf1071846
charlieblindsay <charlieblindsay@gmail.com>
2022-09-20 19:09:19 +0100

    pulled remote

--- src/pages/Press.tsx
+++ src/pages/Press.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/Header'
-import { HeaderText, SubtitleText } from '@/components/Text'
+import { SubtitleText } from '@/components/Text'
 import React from 'react'
 
 import Elgars from '@/assets/images/elgars.png'

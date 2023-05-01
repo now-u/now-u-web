eda8074ce19cc9e8d8cf10a0233b7f607dd8085d
Anton Khomchenko <anton@khomchenko.com>
2023-05-01 06:55:16 +0100

    [FEAT] GTAG added

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { getRequiredEnvironmentVariable } from '@/utils/getRequiredEnvironmentVariable';
+import Script from 'next/script';
+export const GTAG = getRequiredEnvironmentVariable("GTAG");
 
 export default function Head(): JSX.Element {
   return (
@@ -6,6 +9,20 @@ export default function Head(): JSX.Element {
       <title></title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`}
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GTAG}');
+    `}
+      </Script>
     </>
   );
 }

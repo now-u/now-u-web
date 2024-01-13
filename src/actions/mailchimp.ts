144fdbd0ec72496030f5ffa68b4a6e93a5927fda
James Elgar <jamesnelgar@gmail.com>
2024-01-13 21:13:10 +0000

    Fix blog alignment

--- src/actions/mailchimp.ts
+++ src/actions/mailchimp.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import type * as mailchimp from '@mailchimp/mailchimp_marketing';
+import type * as mailchimp from "@mailchimp/mailchimp_marketing";
 import { MAILCHIMP_LIST_ID, mailchimpClient } from "@/services/mailchimp";
 import { z } from "zod";
 
@@ -8,12 +8,14 @@ const subscribeInputSchema = z.object({ email: z.string().email() });
 
 export async function subscribeToNewsLetter(
   input: z.infer<typeof subscribeInputSchema>,
-): Promise<{ result: 'ADDED' | 'VALIDATION_ERROR' | 'ALREADY_SUBSCRIBED' | 'ERROR' }> {
-  console.info('Subscribing user with email=', input.email);
+): Promise<{
+  result: "ADDED" | "VALIDATION_ERROR" | "ALREADY_SUBSCRIBED" | "ERROR";
+}> {
+  console.info("Subscribing user with email=", input.email);
   const result = subscribeInputSchema.safeParse(input);
   if (!result.success) {
     console.error(`Failed to parse email=`, input.email);
-    return { result: 'VALIDATION_ERROR' };
+    return { result: "VALIDATION_ERROR" };
   }
 
   try {
@@ -21,14 +23,14 @@ export async function subscribeToNewsLetter(
       email_address: input.email,
       status: "subscribed",
     });
-    return { result: 'ADDED' };
+    return { result: "ADDED" };
   } catch (e: any) {
     const error = e.response.body as mailchimp.ErrorResponse;
-	if (error.title === "Member Exists") {
-		console.info(`User already subscribed with email=`, input.email, error);
-		return { result: 'ALREADY_SUBSCRIBED' };
-	}
+    if (error.title === "Member Exists") {
+      console.info(`User already subscribed with email=`, input.email, error);
+      return { result: "ALREADY_SUBSCRIBED" };
+    }
     console.error(`Failed to subscribe user with email=`, input.email, error);
-    return { result: 'ERROR' };
+    return { result: "ERROR" };
   }
 }

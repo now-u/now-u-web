1e0dc9abd6e44f0120e0269593a69dbde8166a15
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-09-03 11:14:04 +0100

    Add blog post related endpoint functions - Fix `getOrganisations` error message - Add `getBlogPosts` and `getBlogPosts(postID:number)` to `api.ts`

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ import createClient from "openapi-fetch";
 export type Cause = components["schemas"]["Cause"];
 export type Organisation = components["schemas"]["Organisation"];
 export type Faq = components["schemas"]["Faq"];
+export type BlogPost = components["schemas"]["Blog"];
 
 export const apiClient = createClient<paths>({
   baseUrl: "https://causes.dev.apiv2.now-u.com",
@@ -35,8 +36,34 @@ export async function getOrganisations(): Promise<Organisation[]> {
   const { data, error, response } = await apiClient.GET("/organisations/");
 
   if (!response.ok || error !== undefined) {
-    console.error(`[${response.status}] Failed to fetch faqs: ${error}`);
+    console.error(`[${response.status}] Failed to fetch organisations: ${error}`);
     return [];
+  }
+
+  return data;
+}
+
+export async function getBlogPosts(): Promise<BlogPost[]> {
+  const { data, error, response } = await apiClient.GET("/blogs/");
+
+  if (!response.ok || error !== undefined) {
+    console.error(`[${response.status}] Failed to fetch blog post list: ${error}`);
+    return [];
+  }
+
+  return data;
+}
+
+export async function getBlogPost(postID: number): Promise<BlogPost | null> {
+  const { data, error, response } = await apiClient.GET("/blogs/{id}/", {
+    params: {
+      path: { id: postID }
+    }
+  });
+
+  if (!response.ok || error !== undefined) {
+    console.error(`[${response.status}] Failed to fetch blog post: ${error}`);
+    return null;
   }
 
   return data;

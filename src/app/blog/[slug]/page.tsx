21bc201f7266eb934f8fd8d9bbbbbabffc7707b7
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-09-04 23:49:59 +0100

    Add author profile image placeholder to `AuthorTile` - use `faUserCircle` as author profile image placeholder when profile image src is an empty string - center author profile image to <title + description>: this design doesn't work for very long author bio/description, but works great for current authors.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,25 +5,36 @@ import { getPostBySlug } from "../utils";
 import { type BlogWriter } from "../writers";
 import Image from "next/image";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 // NOTE: https://blog.openreplay.com/creating-a-markdown-blog-powered-by-next-js-in-under-an-hour
 
 function AuthorTile(props: { author: BlogWriter }): JSX.Element {
   const { author } = props;
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="h-32 w-32 mb-6 md:w-96 relative mr-5">
-        <Image
-          src={author.profile_picture_url}
-          alt="Blog Image"
-          className="object-cover w-full h-full rounded-full"
-          sizes="100vw"
-          fill
-        />
+    <div className="flex flex-col mt-10 mb-6 gap-4 md:flex-row md:items-center">
+      <div className="aspect-square h-24 w-24 md:h-32 md:w-32 relative">
+        {author.profile_picture_url !== "" ? (
+          <Image
+            src={author.profile_picture_url}
+            alt="Blog Image"
+            className="object-cover object-center w-full h-full rounded-full m-0"
+            sizes="100vw"
+            fill
+          />
+        ) :
+          <FontAwesomeIcon
+            className="object-cover object-center w-full h-full rounded-full m-0"
+            color="#DDD"
+            icon={faUserCircle}
+          />
+        }
       </div>
       <div>
-        <p className="mb-2 font-bold"> {author.full_name} </p>
-        <p className="mt-0"> {author.description} </p>
+        <p className="m-0 mb-2 font-bold"> {author.full_name} </p>
+        <p className="m-0"> {author.description} </p>
       </div>
     </div>
   );
@@ -44,6 +55,9 @@ export default async function Page({
   params: { slug: string };
 }): Promise<JSX.Element> {
   const blog = await getPostBySlug(params.slug);
+  if (blog === undefined) {
+    notFound();
+  }
 
   return (
     <>

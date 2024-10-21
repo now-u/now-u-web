fe7d34e40fa5f3628739460bbc1153e87c2339ad
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-10-21 23:42:30 +0100

    Changt title to 'Charity Partnership'; collapsible content text vertical spacing adjustment

--- src/app/charity-partnership/page.tsx
+++ src/app/charity-partnership/page.tsx
@@ -15,7 +15,7 @@ export default function PartnershipPage(): React.ReactElement {
     <>
       <title>now-u | Charity Partnership</title>
       <Header
-        title="Partnership"
+        title="Charity Partnership"
         subtitle="Partner with now-u: Empower Change Together"
         body="Charity partnerships are a key part of now-u’s mission to create a more just and sustainable world. To us, partnership means collaboration, shared vision, and collective action. By joining forces with other charities, we amplify our shared impact, leverage diverse expertise, and ensure a more holistic approach to addressing complex global challenges."
       />

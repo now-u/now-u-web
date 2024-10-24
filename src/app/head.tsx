5bc6ba29dfe3788628bb5970b2c4a2be9560d1f9
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-10-25 00:37:42 +0100

    Update icons to use another generator instead

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,8 @@ export default function Head(): JSX.Element {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />

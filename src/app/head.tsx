cbd7252634655bcd62a7ef93c8ac1e366979c69d
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-10-24 23:51:34 +0100

    Use rounded icons

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,12 @@ export default function Head(): JSX.Element {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="now-u" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }

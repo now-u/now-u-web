8ada485c2011263617eea1930f85e5b628b1a301
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-11-01 21:26:23 +0000

    Add partnership page to nav bar

--- src/model/Routes.ts
+++ src/model/Routes.ts
@@ -8,14 +8,14 @@ export const causesRoute: NamedRoute = { text: "Causes", to: "/causes" }
 export const aboutRoute: NamedRoute = { text: "About Us", to: "/about" }
 export const blogRoute: NamedRoute = { text: "Blog", to: "/blog" }
 export const getInTouchRoute: NamedRoute = { text: "Get In Touch", to: "/get-in-touch" }
-
+export const partnershipRoute: NamedRoute = { text: "Partnership", to: "/charity-partnership" }
 export const pressRoute: NamedRoute = { text: "Press", to: "/press" }
 export const cookieRoute: NamedRoute = { text: "Cookie Notice", to: "/info/cookie-policy" }
 export const privacyNoticeRoute: NamedRoute = { text: "Privacy Notice", to: "/info/privacy-notice" }
 export const termsRoute: NamedRoute = { text: "Terms and Conditions for Users", to: "/info/terms-and-conditions-for-users" }
 
 export const navigationRoutes = [
-  homeRoute, causesRoute, aboutRoute, blogRoute, getInTouchRoute
+  homeRoute, causesRoute, aboutRoute, partnershipRoute, blogRoute, getInTouchRoute
 ]
 
 export const mainRoutes = [
@@ -30,6 +30,7 @@ export const legalAndInfoRoutes = [
 
 export const aboutRoutes = [
   aboutRoute,
+  partnershipRoute,
   getInTouchRoute,
   pressRoute
 ]

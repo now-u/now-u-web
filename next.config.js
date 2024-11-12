d7709f9a3fb87fa9bab66fb7f1d564d5a0eb6608
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-11-12 01:56:01 +0000

    Redirect `/app/feedback` and `/app/research/signup` to corresponding google forms

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 const withRoutes = require("nextjs-routes/config")();
 
 // TODO Remove wildcard for remotePatterns and update to only allow images s3 bucket
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/app/feedback",
+        destination: "https://forms.gle/xenzFkRVrELsqnnB9",
+        permanent: true, // Use true for 301 redirect, false for 302
+      },
+      {
+        source: "/app/research/signup",
+        destination: "https://forms.gle/DjSdjH1DfNtRwXiP7",
+        permanent: true, // Use true for 301 redirect, false for 302
+      },
+    ];
+  },
   logging: {
     fetches: {
       fullUrl: true,

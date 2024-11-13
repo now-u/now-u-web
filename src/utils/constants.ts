f71184019f9c4fd50f0275caba8abb06b8f8cda1
Yuhao <16722967+clementinelove@users.noreply.github.com>
2024-11-13 19:23:13 +0000

    Update app store link to show ios screenshots instead

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const APPLE_STORE_URL =
-  "https://apps.apple.com/us/app/now-u/id1516126639";
+  "https://apps.apple.com/gb/app/now-u/id1516126639?platform=iphone";
 export const GOOGLE_STORE_URL =
   "https://play.google.com/store/apps/details?id=com.nowu.app";
 

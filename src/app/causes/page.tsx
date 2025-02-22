054755e60164699564b2b1cd878f37698b9f9b60
Yuhao <16722967+clementinelove@users.noreply.github.com>
2025-02-22 20:36:16 +0000

    fix: Remove unused `CauseTile`

--- src/app/causes/page.tsx
+++ src/app/causes/page.tsx
@@ -2,37 +2,13 @@ import React from "react";
 
 import { Header } from "@/components/Header";
 import { Newsletter } from "@/components/Newsletter";
-import Image from "next/image";
-import { type Cause, getCauses } from "@/services/api";
+import { getCauses } from "@/services/api";
 import type { Metadata } from "next";
 import ContentTile from "@/stories/organisms/ContentTile";
 
 export const metadata: Metadata = {
   title: "Causes | now-u"
 }
-
-const CauseTile = (props: { cause: Cause }): JSX.Element => {
-  const { header_image: headerImage, title } = props.cause;
-  return (
-    <div className="max-w-sm bg-cream rounded-lg h-full ">
-      <Image
-        className="rounded-t-lg h-60 w-full object-cover"
-        src={headerImage.url}
-        alt={title ?? "cause-image"}
-        width={260}
-        height={260}
-      />
-      <div className="p-5">
-        <div className="flex items-center justify-start flex-1 space-x-4">
-          <h2 className="mb-2 text-xl font-bold tracking-tight">{title}</h2>
-        </div>
-        <p className="mb-3 font-normal text-left text-gray-600">
-          {props.cause.description}
-        </p>
-      </div>
-    </div>
-  );
-};
 
 export default async function CausesPage(): Promise<JSX.Element> {
 
